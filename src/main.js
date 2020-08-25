7b15294f93f6ce9b83a3a5da3a84d14bccd9a3ec
takeyourtime-hw <1457328087>
2020-08-25 12:08:39 +0800

    vue文件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+Vue.prototype.$http=Axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{msg}}</h1>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      msg:"法撒结局司法局哦i哇"
+    }
+  },
+  created(){
+    this.$http.get("/api/w/website/bannerList").then(res=>{
+      console.log(res)
+    })
+  }
+}
+</script>
